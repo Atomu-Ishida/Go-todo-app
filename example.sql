73513d70ae95975b684e15c0d3e30259198beb94
Atomu-Ishida <atomu.10.18@icloud.com>
2022-07-19 01:30:19 +0900

    uuidを削除

--- example.sql
+++ example.sql
@@ -4,7 +4,6 @@ drop table todos;
 
 create table users (
   id         serial primary key,
-  uuid       varchar(64) not null unique,
   name       varchar(255),
   email      varchar(255) not null unique,
   password   varchar(255) not null,
