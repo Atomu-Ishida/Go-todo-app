6d81f241dc7a2c6f0a6ddce4666f21f8d481db6b
Atomu-Ishida <atomu.10.18@icloud.com>
2022-07-19 01:37:01 +0900

    uuidを削除

--- example.sql
+++ example.sql
@@ -12,7 +12,6 @@ create table users (
 
 create table sessions (
   id         serial primary key,
-  uuid       varchar(64) not null unique,
   email      varchar(255),
   user_id    integer references users(id),
   created_at timestamp not null
